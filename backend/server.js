10e4b7ac890f3eda55a35f88c2a429e71f4c2c8b
Tikesh <aswaletinku@gmail.com>
2026-02-06 20:02:21 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -11,25 +11,7 @@ const PORT = process.env.PORT || 5000
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
 
 // Middleware
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://cmsassignment.netlify.app/'
-]
-
-app.use(cors({
-  origin: (origin, callback) => {
-    // allow server-to-server & Postman requests
-    if (!origin) return callback(null, true)
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('CORS not allowed'), false)
-    }
-  },
-  credentials: true
-}))
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
